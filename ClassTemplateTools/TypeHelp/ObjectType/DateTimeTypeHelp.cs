d168d54be7b5c30696bf6489318f28ea5158ac97
chengwei.shiu <cheng-wei.shiu@ablerex.com.tw>
2020-05-25 18:34:53 +0800

    Add DateTimeTypeHelp

---
+++ ClassTemplateTools/TypeHelp/ObjectType/DateTimeTypeHelp.cs
@@ -0,0 +1,29 @@
+﻿using ClassTemplateTools.Contract;
+using ClassTemplateTools.TypeHelp.Brackets;
+using System;
+using System.Collections.Generic;
+
+namespace ClassTemplateTools.TypeHelp.ObjectType
+{
+    [TypeName("DateTime")]
+    internal class DateTimeTypeHelp : ITypeHelp
+    {
+        public string BaseName(object obj)
+        => "DateTime";
+
+        public string ReassemblyStringInBrackets(IEnumerable<string> strs, string title)
+        {
+            var brackets = new LineBreakBrackets();
+            return brackets.ReassemblyStringInBrackets(strs, title);
+        }
+
+        public string ToConstructString(object obj)
+        {
+            var date = (DateTime)obj;
+            return $"new DateTime({date.Year}, {ToFillup(date.Month)}, {ToFillup(date.Day)}, {ToFillup(date.Hour)}, {ToFillup(date.Minute)}, {ToFillup(date.Second)}, DateTimeKind.{date.Kind.ToString()})";
+        }
+
+        private string ToFillup(int num)
+        => num.ToString().PadLeft(2, '0');
+    }
+}
---
+++ ClassTemplateToolsTests/TypeHelp/ObjectType/DateTimeTypeTypeHelpTests.cs
@@ -0,0 +1,55 @@
+﻿using ClassTemplateTools;
+using ClassTemplateTools.TypeHelp.ObjectType;
+using ClassTemplateToolsTests.Extensions;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace ClassTemplateToolsTests.TypeHelp.ObjectType
+{
+    public class DateTimeTypeTypeHelpTests
+    {
+        private string CurrentFolder => Path.Combine(
+            Path.GetFullPath(@"..\..\..\"), "TypeHelp/ObjectType/DateTimeTypeHelpTests_ToConstructString_Result.txt");
+
+        [Fact]
+        public void BaseName()
+        {
+            var su = new DateTimeTypeHelp();
+            var result = su.BaseName(new DateTime(2020, 05, 25));
+            result.Should().Be("DateTime");
+        }
+
+        [Fact]
+        public void ToConstructString()
+        {
+            var parttern = File.ReadAllText(CurrentFolder)
+                               .SplitLine("----------------------------")
+                               .First();
+
+            var su = new DateTimeTypeHelp();
+            var result = su.ToConstructString(new DateTime(2020, 05, 25));
+            result.Should().Be(parttern);
+        }
+
+        [Fact]
+        public void ReassemblyStringInBrackets()
+        {
+            var parttern = File.ReadAllText(CurrentFolder)
+                               .SplitLine("----------------------------")
+                               .ToArray()[1];
+
+            var su = new DateTimeTypeHelp();
+            var result = new[]
+            {
+                new DateTime(2020, 05, 25),
+                new DateTime(2020, 05, 26, 01, 26, 17),
+            }.DumpToString();
+            result.Should().Be(parttern);
+        }
+    }
+}
